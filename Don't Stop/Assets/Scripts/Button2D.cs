324ba9dce4c1a65bfaabf04558bfdaeb97a52aad
Jayden <1022721@student.sae.edu>
2022-02-23 17:27:58 +0800

    Tried and Failed to Make button

--- Don't Stop/Assets/Scripts/Button2D.cs
+++ Don't Stop/Assets/Scripts/Button2D.cs
@@ -1,18 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Button2D : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float threshold = 0.1f;
+    [SerializeField] private float deadZone = 0.025f;
+
+    private bool isPressed;
+    private Vector2 startPosition;
+    private SpringJoint2D joint;
+
+    public UnityEvent onPressed, onReleased;
+
     void Start()
     {
-        
+        startPosition = transform.localPosition;
+        joint = GetComponent<SpringJoint2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (!isPressed && GetValue() + threshold >= 1)
+        {
+            Pressed();
+        }
+
+        if (isPressed && GetValue() - threshold <= 0)
+        {
+            Released();
+        }
+    }
+
+    private float GetValue()
+    {
+        var value = Vector2.Distance(startPosition, transform.localPosition);
+
+        if (Math.Abs(value) < deadZone)
+        {
+            value = 0;
+        }
+
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+    private void Pressed()
+    {
+        isPressed = true;
+        onPressed.Invoke();
+    }
+
+    private void Released()
+    {
+        isPressed = false;
+        onReleased.Invoke();
     }
 }
